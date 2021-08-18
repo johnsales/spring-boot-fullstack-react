32cf65da04c975a209cecdb1a7bfbaa45dbba8b4
John Santiago <jmsvsmorone@hotmail.com>
2021-08-18 15:54:25 -0500

    first commit

---
+++ src/main/java/edu/john/student/Gender.java
@@ -0,0 +1,7 @@
+package edu.john.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/edu/john/student/Student.java
@@ -0,0 +1,15 @@
+package edu.john.student;
+
+import lombok.*;
+
+@ToString
+@Getter @Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Gender gender;
+}
