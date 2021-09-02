bb06713df6641e1d95d8c89ae010e3e8106ded71
John Santiago <jmsvsmorone@hotmail.com>
2021-09-01 22:15:38 -0500

    adding missing files

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+@AllArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllStudents(Pageable pageable) {
+        return new ResponseEntity<>(studentService.getAllStudents(PageRequest.of(0, 100)), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public void addStudent(@Valid @RequestBody Student student) {
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+}
